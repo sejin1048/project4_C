e626dec771bbfeecafc3d2e0776a0004a1083fe8
sejin1048 <hsjin1048@gmail.com>
2025-02-26 13:53:47 +0900

    초기 커밋.

---
+++ project1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+//plus function
+int add(int a, int b) {
+	return a + b;
+}
+
+int main() {
+	//return 0; //exit code 0
+	int result = add(3, 5); //procedure1
+	int result2 = add(4, 5); //procedure2
+	cout << "Hello World!\n";
+	cout << "nice to meet you\n";
+	cout << result;
+	cout << result2;
+}
+
+
+
