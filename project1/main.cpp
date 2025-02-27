6d4662268d01edc556d370560613f01d34fe587f
sejin1048 <hsjin1048@gmail.com>
2025-02-27 09:32:36 +0900

    modified

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream>  //
 using namespace std;
 
 
@@ -11,11 +11,13 @@ int main() {
 	//return 0; //exit code 0
 	int result = add(3, 5); //procedure1
 	int result2 = add(4, 5); //procedure2
-	cout << "Hello World!\n";
-	cout << "nice to meet you\n";
-	cout << result;
-	cout << result2;
+	cout << "Hello World!" << endl;
+	cout << "nice to meet you" << endl;
+	//cout << result;
+	//cout << result2;
 }
+
+
 
 
 
